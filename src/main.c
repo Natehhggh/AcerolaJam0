336ec60bdab2eae5c817309af1b75c99e09495b9
Natehhggh <natehhggh@gmail.com>
2024-03-02 17:28:07 -0800

    adding initial 3d camera and object rendering

--- src/main.c
+++ src/main.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
 #include "raylib.h"
 
+Camera3D camera = {0};
+Vector3 playerPosition = {0,0,0};
+
+void initCamera(){
+    camera.position = (Vector3){2.5f, 2.5f,3.0f};
+    camera.target = (Vector3){0.0f, 0.0f, 0.7f};
+    camera.up = (Vector3){0.0f,1.0f,0.0f};
+    camera.fovy = 65.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+}
+
+
+
+
 int main() {
     InitWindow(800,800,"Jam Game");
 
     SetTargetFPS(60);
+    initCamera();
 
     while (!WindowShouldClose())
     {
 
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        BeginMode3D(camera);
 
+        DrawCube(playerPosition, 1.0f,1.0f,1.0f, BLUE);
+
+        DrawGrid(10,1.0f);
+        EndMode3D();
+
+        DrawFPS(10,10);
         EndDrawing();
+
     }
 
 
     return 0;
 }
+
+
