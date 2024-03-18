b67f914d589abe57defe22db19fe386d546f2025
Natehhggh <natehhggh@gmail.com>
2024-03-17 20:14:14 -0700

    basic ECS setup:

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ https://www.youtube.com/watch?v=GlPREQ55kzc
 Made in Raylib with C.
 Unsure at this moment how likely I will finish on time, as it's my first major project
 in Raylib, and It's been a while since using c/c++.
+Update 3/12, Yeah, not finishing, stuff came up, but I'm going to keep working
+on this to get some things figured out
 
 ### Current Game Idea
 - Simple? Space flying sim, around a Topopolis megastructure around a star
--- src/main.c
+++ src/main.c
@@ -1,37 +1,85 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <stdint.h>
+#include <string.h>
+//#include "player.c"
 
-Camera3D camera = {0};
 
-
-struct Player {
+//TODO: Move structs to header
+struct transform {
+    int8_t entityId;
     Vector3 position;
     Vector3 rotation;
     Vector3 scale;
 };
 
-void initCamera(){
-    camera.position = (Vector3){-3.0f, 2.5f,0.0f};
-    camera.up = (Vector3){0.0f,1.0f,0.0f};
-    camera.fovy = 65.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-}
-
-Vector3 cameraOffset = {0};
-
-struct Player player = {
-    .position = {0.0f,0.0f,0.0f},
-    .rotation = {0.0f,0.0f,0.0f},
-    .scale = {1.5f,1.0f,1.0f}
+struct trackingCamera{
+    int8_t transformId;
+    Camera3D camera;
+    Vector3 offset;
 };
 
+
+struct entity{
+    int8_t id;
+    char debugName[10];
+};
+
+struct entityInput{
+    int8_t entityId;
+    int8_t transformId;
+};
+
+
+//-- Objects --
+struct entity entities[100];
+struct transform transforms[100];
+struct trackingCamera camera = {0};
+struct entityInput input = {0};
+
+
+//Good Enough, just assuming I have 1 camera for now, and it's what it is
+void initCamera(){
+    camera.camera.position = (Vector3){-3.0f, 2.5f,0.0f};
+    camera.camera.up = (Vector3){0.0f,1.0f,0.0f};
+    camera.camera.fovy = 65.0f;
+    camera.camera.projection = CAMERA_PERSPECTIVE;
+    camera.transformId = 0;
+    camera.offset = Vector3Subtract(camera.camera.position, transforms[camera.transformId].position);
+}
+
+
+struct trackingCamera updateTrackingCamera(struct trackingCamera camera){
+    camera.camera.position = Vector3Add(transforms[camera.transformId].position, camera.offset);
+    camera.camera.target = transforms[camera.transformId].position;
+    return camera;
+}
+
+
+
+void initEntities(){
+
+    entities[0].id = 0;
+    strcpy(entities[0].debugName, "Player");
+    
+    transforms[0].position = (Vector3){0.0f,0.0f,0.0f};
+    transforms[0].rotation = (Vector3){0.0f,0.0f,0.0f};
+    transforms[0].scale = (Vector3){2.5f,1.0f,1.0f};
+    transforms[0].entityId = 0;
+    
+    input.transformId = 0;
+    input.entityId = transforms[input.transformId].entityId;
+
+}
 
 void InitScene(){
     InitWindow(800,800,"Jam Game");
     SetTargetFPS(60);
+    initEntities();
     initCamera();
-    cameraOffset = Vector3Subtract(camera.position,player.position);
 }
+
+
 
 void DrawScene(){
     BeginDrawing();
@@ -39,9 +87,10 @@ void DrawScene(){
 
         ClearBackground(RAYWHITE);
 
-        BeginMode3D(camera);
+        BeginMode3D(camera.camera);
         {
-            DrawCubeV(player.position, player.scale, BLUE);
+            //TODO: Render struct 
+            DrawCubeV(transforms[0].position, transforms[0].scale, BLUE);
 
             DrawGrid(10,1.0f);
         }
@@ -51,21 +100,19 @@ void DrawScene(){
     EndDrawing();
 
 }
-            
 
-int main()
-{
-    InitScene();
-
-    while(!WindowShouldClose())
-    {
+void HandleInput(struct entityInput playerInput){
+    
+        float playerSpeed = 10.0f * GetFrameTime(); 
         Vector3 playerInputDir = {0.0f,0.0f,0.0f};
+
         if(IsKeyDown(KEY_W)){
             playerInputDir.x = 1.0f;
         }
         if(IsKeyDown(KEY_A)){
             playerInputDir.z = -1.0f;
         }
+        //camera.position = Vector3Add(camera.position, playerInputDir);
         if(IsKeyDown(KEY_S)){
             playerInputDir.x = -1.0f;
         }
@@ -73,15 +120,23 @@ int main()
             playerInputDir.z = 1.0f;
         }
 
-
-
-        float playerSpeed = 10.0f * GetFrameTime(); 
         playerInputDir = Vector3Normalize(playerInputDir);
         playerInputDir = Vector3Scale(playerInputDir, playerSpeed);
-        player.position = Vector3Add(playerInputDir, player.position);
-        camera.position = Vector3Add(player.position, cameraOffset);
+        transforms[playerInput.transformId].position = Vector3Add(
+                playerInputDir, transforms[playerInput.transformId].position);
 
-        camera.target = player.position;
+}
+
+
+int main()
+{
+
+    InitScene();
+
+    while(!WindowShouldClose())
+    {
+        HandleInput(input);
+        camera = updateTrackingCamera(camera);
         DrawScene();
     }
 
