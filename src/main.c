6eb4050628d33ae3f0a84f03cf5bce50006499ad
Natehhggh <natehhggh@gmail.com>
2024-03-19 03:03:51 -0700

    better entity management, bug with movement

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 
-#define MAX_ENTITIES 10
+#define MAX_ENTITIES 100
 
 
 //FPS Notes:
@@ -21,7 +21,9 @@ enum shapeType {
 };
 
 //TODO: Move structs to header
-
+//TODO: get component by Id methods for camera and controllers
+//TODO: sort entities by active/alive
+//TODO: swap bools to mask flags
 struct trackingCamera{
     int8_t transformId;
     Camera3D camera;
@@ -66,12 +68,12 @@ struct entityInput input = {0};
 
 
 //Good Enough, just assuming I have 1 camera for now, and it's what it is
-void initCamera(){
+void initCamera(int8_t transformId){
     camera.camera.position = (Vector3){-3.0f, 2.5f,0.0f};
     camera.camera.up = (Vector3){0.0f,1.0f,0.0f};
     camera.camera.fovy = 65.0f;
     camera.camera.projection = CAMERA_PERSPECTIVE;
-    camera.transformId = 0;
+    camera.transformId = transformId;
     camera.offset = Vector3Subtract(camera.camera.position, transforms[camera.transformId].position);
 }
 
@@ -82,31 +84,82 @@ struct trackingCamera updateTrackingCamera(struct trackingCamera camera){
     return camera;
 }
 
+int8_t newEntity(char name[10]){
+    int8_t i = 0;
+    while(entities[i].id < 0){
+        ++i;
+    }
+    entities[i].id = i;
+    strcpy(entities[i].debugName, name);
+    return i;
+}
+
+int8_t newTransform(int8_t entityID, Vector3 position, Vector3 rotation, Vector3 scale){
+    int8_t i = 0;
+    while(transforms[i].entityId < 0){
+        ++i;
+    }
+    transforms[i].entityId = entityID;
+    transforms[i].position = position;
+    transforms[i].rotation = rotation;
+    transforms[i].scale = scale;
+
+    return i;
+}
+
+int8_t newShapeRender(int8_t entityId, int8_t transformId, enum shapeType shape, bool isActive){
+    int8_t i = 0;
+    while(shapeRenders[i].transformId < 0){
+        ++i;
+    }
+    shapeRenders[i].entityId = entityId;
+    shapeRenders[i].transformId = transformId;
+    shapeRenders[i].shape = shape;
+    shapeRenders[i].isActive = isActive;
+    return i;
+}
 
 
-void initPlayer(){
+void releaseEntity(int8_t entityId){
+    entities[entityId].id = -128;
+    for(int i = 0; i < MAX_ENTITIES; ++i){
+        if(transforms[i].entityId == entityId){
+        transforms[i].entityId = -127;
+        }
+        if(shapeRenders[i].entityId == entityId){
+            shapeRenders[i].entityId = -126;
+            shapeRenders[i].transformId = -126;
+            shapeRenders[i].isActive = false;
+        }
+    }
 
 }
 
 
+void setDefaultData(){
+    //Set useful data to unlinked negative values;
+    for(int i = 0; i < MAX_ENTITIES; ++i){
+        entities[i].id = -128;
+        transforms[i].entityId = -127;
+        shapeRenders[i].entityId = -126;
+        shapeRenders[i].transformId = -126;
+    }
+}
 
+void initPlayer(){
+    int8_t entityId = newEntity("Player");
+    int8_t transformId = newTransform(entityId, (Vector3){0.0f,0.0f,0.0f}, (Vector3){0.0f,0.0f,0.0f}, (Vector3){2.5f,1.0f,1.0f});
+    int8_t shapeId = newShapeRender(entityId, transformId, Cube, true);
+    
+    input.transformId = transformId;
+    input.entityId = transforms[input.transformId].entityId;
+    initCamera(transformId);
+}
 
 void initEntities(){
 
-    entities[0].id = 0;
-    strcpy(entities[0].debugName, "Player");
-    
-    transforms[0].position = (Vector3){0.0f,0.0f,0.0f};
-    transforms[0].rotation = (Vector3){0.0f,0.0f,0.0f};
-    transforms[0].scale = (Vector3){2.5f,1.0f,1.0f};
-    transforms[0].entityId = 0;
-    shapeRenders[0].shape = Cube;
-    shapeRenders[0].transformId = 0;
-    shapeRenders[0].entityId = transforms[shapeRenders[0].transformId].entityId;
-    shapeRenders[0].isActive = true;
-    
-    input.transformId = 0;
-    input.entityId = transforms[input.transformId].entityId;
+    setDefaultData();
+    initPlayer();
 
 }
 
@@ -114,7 +167,6 @@ void InitScene(){
     InitWindow(800,800,"Jam Game");
 //    SetTargetFPS(60);
     initEntities();
-    initCamera();
 }
 
 
@@ -188,7 +240,6 @@ void HandleInput(struct entityInput playerInput){
         if(IsKeyDown(KEY_D)){
             playerInputDir.z = 1.0f;
         }
-
         playerInputDir = Vector3Normalize(playerInputDir);
         playerInputDir = Vector3Scale(playerInputDir, playerSpeed);
         transforms[playerInput.transformId].position = Vector3Add(playerInputDir, transforms[playerInput.transformId].position);
